c236a177b606deaf271c7c1c125d6878a42ee472
Michael Agee <michael.agee@homes.com>
2015-10-15 21:55:51 -0400

    Added Angular

--- public/javascript/test.js
+++ public/javascript/test.js
@@ -1,2 +1,33 @@
 var a = 'Hello world';
 console.log(a);
+var ngModule = angular.module;
+ngModule('hrApp', [
+	'home'
+]);
+
+ngModule('home', [])
+.directive('homePage', function () {
+        return {
+            restrict: 'E',
+            bindToController: true,
+            controller: 'homePageController',
+            controllerAs: 'homePageCtrl',
+            template: '<div><p>{{homePageCtrl.name}}</p></div>',
+            replace: true,
+            scope: {
+
+            }
+        }
+})
+.controller('homePageController', [
+    '$scope',
+    function ($scope) {
+        console.log($scope, 'scope');
+        var self = this;
+        self.name = 'ragefukingshit';
+    }
+]);
+
+angular.element(document).ready(function () {
+	angular.bootstrap(document, ['hrApp']);
+});
