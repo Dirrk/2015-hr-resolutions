e89ba2527a6406bd3e0ff63aa810b11772f973d2
Paul Jeffreys <paul.jeffreys@homes.com>
2015-10-23 12:56:34 -0400

    fleshing out pages and adding custom css file

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -13,7 +13,7 @@ ngModule('globalNav', [])
         return {
             restrict: 'E',
             replace: true,
-            templateUrl: 'views/global-nav.html'
+            templateUrl: '../views/global-nav.html'
         }
     });
 
