c17ca6a2b59d90980b095ef99c61aa4dbe4fe3f1
Paul Jeffreys <paul.jeffreys@homes.com>
2015-10-23 10:25:07 -0400

    Rolling everything back

--- public/javascript/app.js
+++ public/javascript/app.js
@@ -3,63 +3,63 @@ console.log(a);
 
 var ngModule = angular.module;
 ngModule('hrApp', [
-	'home',
-	'about',
-	'globalNav'
+    'home',
+    'about',
+    'globalNav'
 ]);
 
 ngModule('globalNav', [])
-	.directive('globalNav', function () {
-		return {
-			restrict: 'E',
-			replace: true,
-			templateUrl: 'views/global-nav.html'
-		}
-	});
+    .directive('globalNav', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'views/global-nav.html'
+        }
+    });
 
 ngModule('home', [])
-	.directive('homePage', function () {
-	        return {
-	            restrict: 'E',
-	            bindToController: true,
-	            controller: 'homePageController',
-	            controllerAs: 'homePageCtrl',
-                templateUrl: '../templates/home.html',
-                replace: true,
-	            scope: {
-
-	            }
-	        }
-	})
-	.controller('homePageController', [
-	    '$scope',
-	    function ($scope) {
-	        var self = this;
-	        self.name = 'ragefukingshit';
-	    }
-	]);
-
-ngModule('about', [])
-	.directive('aboutPage', function () {
-		return {
-			restrict: 'E',
+    .directive('homePage', function () {
+        return {
+            restrict: 'E',
             bindToController: true,
-            controller: 'aboutPageController',
-            controllerAs: 'aboutPageCtrl',
-            templateUrl: '../templates/about.html',
+            controller: 'homePageController',
+            controllerAs: 'homePageCtrl',
+            template: '<div><p>{{homePageCtrl.name}}</p></div>',
             replace: true,
             scope: {
 
             }
-		}
-	})
-	.controller('aboutPageController', [
-		'$scope',
-		function ($scope) {
-			var self = this;
-			self.title = 'yay wtf';
-			console.log('about page scope');
-		}]);
+        }
+    })
+    .controller('homePageController', [
+        '$scope',
+        function ($scope) {
+            var self = this;
+            self.name = 'ragefukingshit';
+        }
+    ]);
+
+ngModule('about', [])
+    .directive('aboutPage', function () {
+        return {
+            restrict: 'E',
+            bindToController: true,
+            controller: 'aboutPageController',
+            controllerAs: 'aboutPageCtrl',
+            template: '<div><p>{{aboutPageCtrl.title}}</p></div>',
+            replace: true,
+            scope: {
+
+            }
+        }
+    })
+    .controller('aboutPageController', [
+        '$scope',
+        function ($scope) {
+            var self = this;
+            self.title = 'yay wtf';
+            console.log('about page scope');
+        }]);
 // html 5 mode on
 // ngModule.config([
 //     '$locationProvider',
@@ -70,5 +70,5 @@ ngModule('about', [])
 // bootstrapping the application this way avoids clutting up the
 // home page with 'ngapp=hrapp'
 angular.element(document).ready(function () {
-	angular.bootstrap(document, ['hrApp']);
+    angular.bootstrap(document, ['hrApp']);
 });
